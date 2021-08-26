b26be058220b998984b98e89c1550fba804e15e8
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-26 14:54:13 -0600

    Stylesheet link?

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ app.use(express.json())
 app.get('/' , (req,res) =>{
     res.sendFile(path.join(__dirname, "../index.html"))
 })
+app.use('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
 
 const port = process.env.PORT || 4005
 
