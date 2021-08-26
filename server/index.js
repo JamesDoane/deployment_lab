03807a59c7f98f5c60dce226bb5a112b39c995b6
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-26 14:46:03 -0600

    dslkfjsldkf

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 
 app.get('/' , (req,res) =>{
     res.sendFile(path.join(__dirname, "../index.html"))
+    res.sendFile(path.join(__dirname, "../styles.css"))
 })
 
 const port = process.env.PORT || 4005
